8cfb3dc69d4d5ef62d1fefc4e96bc02937f92815
ivileey <nishuiaee@126.com>
2020-05-18 14:50:29 +0800

    BruteForce

--- src/String/BruteForce.java
+++ src/String/BruteForce.java
@@ -1,12 +1,14 @@
 package String;
-
+/*
+*   Brute-Force 一种有回溯的字符串匹配算法
+* */
 public class BruteForce {
 
     public int search(String target, String pattern){
         int target_len = target.length();
         int parttern_len = pattern.length();
 
-       for(int i = 0; i < target_len; i++){
+       for(int i = 0; i < target_len && i+parttern_len < target_len; i++){
            if(target.substring(i, i + parttern_len).equals(pattern))
            {
                return i;
